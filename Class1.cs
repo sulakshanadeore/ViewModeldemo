5eb9b83fef8c3376ca6a83f8b077f936667c45a4
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2022-10-06 13:01:29 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ViewModeldemo.Models
+{
+    public class CustModel
+    {
+        [Required()]
+        public int Custid { get; set; }
+        [MaxLength(10, ErrorMessage = "Name cannot be greater than 10 characters")]
+        [MinLength(3, ErrorMessage = "Name cannot be less than 3 character")]
+        public string Custname { get; set; }
+        public string City { get; set; }
+
+
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime RegistrationDate { get; set; }
+    }
+}
