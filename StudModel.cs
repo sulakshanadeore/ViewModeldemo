32e08fee00c79f58379b618b78c3d63ee6c8a884
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2022-10-06 16:04:54 +0530

    Add files via upload

---
+++ StudModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewModeldemo.Models
+{
+    public class StudModel
+    {
+        public int Rollno { get; set; }
+        public string Studname { get; set; }
+    }
+}
